211730350107b92e0bfe4f8b8e053ad3c0f51034
GhalyDoaa <doaaghaly397@gmail.com>
2020-05-08 16:29:55 -0400

    edit database configs phase 3

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-$ export DATABASE_URL="postgres://yxtbfhiimrmpkb:cba616d93a15630b2ee38c27bb1a47436dc718c809c9308d9afd9d628ff1603e@ec2-54-247-89-181.eu-west-1.compute.amazonaws.com:5432/d23fp4k7pk883m"
+$ export DATABASE_URL="postgres://hcattzyekzezab:0bac34edcfae5555066a87ac9c79faba57b7253d225a84836b99f2ef45ff9183@ec2-52-202-146-43.compute-1.amazonaws.com:5432/d904ubfi789uqa"
