9982e8a808ce9046136750543f7539052285cadb
GhalyDoaa <doaaghaly397@gmail.com>
2020-05-09 18:02:18 -0400

    edit db3

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from models import setup_db,db_create_all ,Movie ,Actor,movies_actors_association
+from models import setup_db,db_create_all ,Movie ,Actor
 from auth import AuthError, requires_auth
 
 ACTOR_PER_PAGE = 10
